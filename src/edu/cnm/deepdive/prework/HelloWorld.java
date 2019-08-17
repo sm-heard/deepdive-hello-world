c3bd25c090b50d4c577753c7630f16d6b0eb9637
Sami <samimheard@gmail.com>
2019-08-16 20:44:13 -0600

    third commit

--- src/edu/cnm/deepdive/prework/HelloWorld.java
+++ src/edu/cnm/deepdive/prework/HelloWorld.java
@@ -1,5 +1,14 @@
 package edu.cnm.deepdive.prework;
 
 public class HelloWorld {
+  public static void main(String[] args) {
+    salute(salutation(args));
+  }
+  private static String salutation(String[] args){
+    return "Hello, " + ((args.length > 0) ? args[0] : "World");
+  }
+  private static void salute(String salutation) {
 
+    System.out.println(salutation);
+  }
 }
